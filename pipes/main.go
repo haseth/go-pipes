61a0607466046eb016e55c1a052a8f6dcaa3cc42
haseth <haseth@cisco.com>
2019-09-19 10:30:27 +0530

    Made a change from *os.File to *bytes.Buffer.
    Details:
    - There were issues coming with working with files as one channel keeping hold of the *file and not allowing other to open it.
    - So, we changed the type from *file to bytes.Buffer.
    - Still refactoring needs to be done on code.

--- pipes/main.go
+++ pipes/main.go
@@ -1,52 +1,61 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"os"
 	"os/exec"
-	"strconv"
 )
 
 //Node make input file
 type Node struct {
-	stdin  *os.File
+	stdin  *bytes.Buffer
 	cmd    []string
-	stdout *os.File
+	stdout *bytes.Buffer
 }
 
 //TakeInputOutputFile ...
-func (n *Node) TakeInputOutputFile(ip chan *os.File) {
-	//take stdin file
+func (n *Node) TakeInputOutputFile(ip chan *bytes.Buffer) {
+	//take stdibytes.Buffer
 	n.stdin = <-ip
-	fmt.Println(" Taken the file")
+	fmt.Println(" Taken the buffer")
 }
 
 //Process ...
-func (n *Node) Process(op chan *os.File, index int) {
+func (n *Node) Process(op chan *bytes.Buffer, index int) {
 	//process the command  by setting the input/output streaming file
+	fmt.Println("Got command")
 	execCmd := exec.Command(n.cmd[0], n.cmd[1:]...)
-	execCmd.Stdin = n.stdin
-	_, err := os.Open(n.stdin.Name())
-	if err != nil {
-		fmt.Println("Error in opening file" + err.Error())
+	//execCmd.Run()
+	if index != 0 {
+		execCmd.Stdin = n.stdin
 	}
-	//make a stdout file
-	s := "temp" + strconv.Itoa(index+1)
-	fmt.Println("Testing name " + s)
-	fileOutput, err := os.Create(s)
+	fmt.Println("Stdin:" + n.stdin.String())
+	//So we have the inpubytes.Buffer for input stream.
+	// _, err := os.Open(n.stdin.Name())
+	// if err != nil {
+	// 	fmt.Println("Error in opening file" + err.Error())
+	// }
 
+	//make a stdoubytes.Buffer and attach as stdout
+	// s := "temp" + strconv.Itoa(index+1)
+	// fmt.Println("Testing name " + s)
+	// fileOutput, err := os.Create(s)
+	outputBuffer := new(bytes.Buffer)
+	n.stdout = outputBuffer
+	fmt.Println("Stdout:" + n.stdout.String())
+	// execCmd.Stdout = os.Stdout
+	// execCmd.Stderr = os.Stderr
+	execCmd.Stdout = outputBuffer
+	//execCmd.Stderr = fileOutput
+
+	err := execCmd.Run()
 	if err != nil {
-		fmt.Println("Error in creating file" + err.Error())
-
+		fmt.Println("Error in running file " + err.Error())
+	} else {
+		//fmt.Println(string(b))
 	}
+	fmt.Println("Runed the command.")
 
-	execCmd.Stdout = fileOutput
-
-	err = execCmd.Run()
-	if err != nil {
-		fmt.Println("Error in reading file" + err.Error())
-	}
-	n.stdout = fileOutput
 	op <- n.stdout
 	// make a output file where we will put the output
 	// n.stdout= testFile
@@ -55,7 +64,7 @@ func (n *Node) Process(op chan *os.File, index int) {
 }
 
 //OutputFile ...
-func (n *Node) OutputFile(op chan *os.File) {
+func (n *Node) OutputFile(op chan *bytes.Buffer) {
 	op <- n.stdout
 }
 
@@ -63,35 +72,31 @@ func main() {
 
 	//commands := "cat test.txt | grep -i test"
 	commandsArray := [][]string{
-		//[]string{"echo", "hello"},
 		[]string{"cat", "/Users/haseth/testing/go-pipes/pipes/test.txt"},
-		[]string{"grep", "-i", "harsh"},
-		[]string{"ls"},
-		[]string{"sort"},
-		[]string{"less"},
+		[]string{"grep", "-i", "lopsem"},
+		// []string{"ls", "-l"},
+		// []string{"sort"},
+		// []string{"less"},
 	}
 	lenOfCmdArray := len(commandsArray)
 	//exec.Command("ls").String()
 
-	//Array of channels of os.File
-	var channels []chan *os.File
-	channels = make([]chan *os.File, lenOfCmdArray+1)
+	//Array of channels of bytes.Buffer
+	var channels []chan *bytes.Buffer
+	channels = make([]chan *bytes.Buffer, lenOfCmdArray+1)
 	//var f *os.File
 
 	for index := range channels {
-		channels[index] = make(chan *os.File)
+		channels[index] = make(chan *bytes.Buffer)
 	}
 
 	//first stdin file
-	go func(channels []chan *os.File) {
-		f, err := os.Create("temp123")
-
-		if err != nil {
-			fmt.Println("Error in creating file" + err.Error())
-		}
-		channels[0] <- f
+	go func(channels []chan *bytes.Buffer) {
+		b := new(bytes.Buffer)
+		channels[0] <- b
 	}(channels)
 
+	//all good till here ..............
 	//testing
 	// index := 0
 	// go commandStruct(commandsArray[0], channels[index], channels[index+1])
@@ -99,15 +104,14 @@ func main() {
 
 	temp := 0
 	for index, cmd := range commandsArray {
-		//for each command
-		//new struct for each command with
+		//for each command new struct for each command with
 		//input channels[i] as ip and channels[i+1] as outputs
 		go commandStruct(cmd, channels[index], channels[index+1], index)
 		temp = index + 1
 	}
-
+	//last channel should retrieve the output.
 	f := <-channels[temp]
-	fmt.Println(f.Name())
+	fmt.Println(f.String())
 
 	// var b []byte
 	// b = make([]byte, 10)
@@ -118,12 +122,10 @@ func main() {
 	// }
 	// fmt.Println("No. of chars", n)
 	// fmt.Println(string(b))
-
 }
 
-func commandStruct(command []string, ip, op chan *os.File, index int) {
+func commandStruct(command []string, ip, op chan *bytes.Buffer, index int) {
 	cmdStruct := &Node{cmd: command}
 	cmdStruct.TakeInputOutputFile(ip)
 	cmdStruct.Process(op, index)
-
 }
